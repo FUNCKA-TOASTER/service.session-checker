2c23c4e5a3ca945efa81bbfe12cb67584494f37b
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-04-28 17:20:26 +1000

    .\config\

---
+++ config/__init__.py
@@ -0,0 +1,28 @@
+"""Module "config".
+About:
+    Service configurations and settings.
+"""
+
+from .config import (
+    TOKEN,
+    GROUP_ID,
+    SERVICE_NAME,
+    API_VERSION,
+    MY_SQL_HOST,
+    MY_SQL_PORT,
+    MY_SQL_PSWD,
+    MY_SQL_USER,
+)
+
+
+__all__ = (
+    "QUEUE_BROKER_IP",
+    "TOKEN",
+    "GROUP_ID",
+    "SERVICE_NAME",
+    "API_VERSION",
+    "MY_SQL_HOST",
+    "MY_SQL_PORT",
+    "MY_SQL_PSWD",
+    "MY_SQL_USER",
+)
---
+++ config/config.py
@@ -0,0 +1,16 @@
+"""Module "config"."""
+
+import os
+
+SERVICE_NAME = "toaster.session-checker-service"
+
+
+TOKEN: str = os.getenv("TOKEN")
+GROUP_ID: int = int(os.getenv("GROUPID"))
+API_VERSION: str = "5.199"
+
+
+MY_SQL_HOST = os.getenv("SQL_HOST")
+MY_SQL_PORT = int(os.getenv("SQL_PORT"))
+MY_SQL_USER = os.getenv("SQL_USER")
+MY_SQL_PSWD = os.getenv("SQL_PSWD")
