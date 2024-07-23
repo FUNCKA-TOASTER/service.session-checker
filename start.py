a4ed5aea1d49d15fe0b1de041b37e448a01dcde7
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-24 00:44:02 +1000

    start script

--- start.py
+++ start.py
@@ -7,18 +7,36 @@ Author:
     oidahomain@gmail.com
 """
 
-import asyncio
-from handler import session_handler
-from logger import logger
+import sys
+import time
+import config
+from loguru import logger
+from data import TOASTER_DB
+from handler import SessionHandler
 
 
-async def main():
+def setup_logger() -> None:
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <red>{module}</red> | <level>{level}</level> | {message}",
+        level="DEBUG",
+    )
+
+
+def main():
     """Entry point."""
-    log_text = "Starting session checking..."
-    await logger.info(log_text)
 
-    await session_handler()
+    setup_logger()
+    TOASTER_DB.create_tables()
+    start_checking = SessionHandler()
+
+    logger.info("Starting session checking...")
+    while True:
+        start_checking()
+        time.sleep(config.ITERRATION_DELAY)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
