4bce6c8534dcf8505802e42589c3048388099583
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-07-24 01:29:15 +1000

    New scripts

--- data/scripts/session.py
+++ data/scripts/session.py
@@ -1,7 +1,11 @@
+from typing import List, Tuple
 from sqlalchemy.orm import Session
 from toaster.database import script
-from data import Session as MenuSession, Delay
+from data import Session as MenuSession, Delay, Peer, PeerMark
 from datetime import datetime, timedelta
+
+
+ExpiredSessions = List[Tuple[int, List[int]]]
 
 
 @script(auto_commit=False, debug=True)
@@ -19,3 +23,27 @@ def close_menu_session(session: Session, bpid: int, cmid: int) -> None:
     menu_session = session.get(MenuSession, {"bpid": bpid, "cmid": cmid})
     session.delete(menu_session)
     session.commit()
+
+
+@script(auto_commit=False, debug=True)
+def get_expired_sessions(session: Session) -> ExpiredSessions:
+    result = []
+    peers = session.query(Peer).filter(Peer.mark == PeerMark.CHAT).all()
+    for peer in peers:
+        cmids = []
+        expired_sessions = (
+            session.query(MenuSession)
+            .filter(MenuSession.expired <= datetime.now())
+            .all()
+        )
+
+        if not expired_sessions:
+            continue
+
+        for expired_session in expired_sessions:
+            cmids.append(expired_session.cmid)
+
+        peer_expired_sessions = (peer, cmids)
+        result.append(peer_expired_sessions)
+
+    return result
